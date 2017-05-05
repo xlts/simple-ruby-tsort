b55967ddecdfa4a0fd38b0455a4b8a0f8df9a730
xlts <xelitas@gmail.com>
2017-05-05 18:52:24 +0100

    fix indentation

--- course_validator.rb
+++ course_validator.rb
@@ -1,24 +1,24 @@
 require_relative 'graph'
 
 class CourseValidator
-	def initialize(file_path)
-		@nodes = []
-		File.readlines(file_path)[1..-1].each do |line|
-			@nodes << line.split(' ')
-		end
-	end
+  def initialize(file_path)
+    @nodes = []
+    File.readlines(file_path)[1..-1].each do |line|
+      @nodes << line.split(' ')
+    end
+  end
 
-	def course_valid?
-		graph.acyclic?
-	end
+  def course_valid?
+    graph.acyclic?
+  end
 
-	def get_order
-		graph.order
-	end
+  def get_order
+    graph.order
+  end
 
 private
-	
-	def graph
-		@graph ||= Graph.new(@nodes)
-	end
+  
+  def graph
+    @graph ||= Graph.new(@nodes)
+  end
 end
